0c2e70926f66a178a31924629a60724b801e58ce
amukhin05 <amukhin@bu.edu>
2025-09-12 13:07:46 -0400

    modified assign 1 perfect power

--- assigns/assign1/lib/assign1.ml
+++ assigns/assign1/lib/assign1.ml
@@ -6,8 +6,10 @@ let num_factors (n : int) : int =
   in count n 2 0
 
   let perfect_power (i : int) (n : int) : bool = 
-    if n = 0 then true
+    if n = 0 && i = 0 then false
+    else if n = 0 then true
     else if n = 1 then true
+    else if i < 0 then false
     else if i = 1 then true
     else if n < 0 then 
       if i mod 2 = 0 then false
